ca25d2443965c78dd0230636761216d1ba8d2cd2
Haciibrahimli <haci.ibrahimli@gmail.com>
2023-11-29 20:45:41 +0200

    lastshenges

--- my_app/urls.py
+++ my_app/urls.py
@@ -8,4 +8,22 @@ urlpatterns = [
     path("gym/<id>/", GYMRetrieveAPIView.as_view(), name='detail'),
     path("gymdelate/<id>/", GYMDestroyAPIView.as_view(), name='delate'),
 
+    path("trainercreated/", TrainerCreateAPIView.as_view(), name='trainer-created'),
+    path("trainerupdate/<id>/", TrainerUpdateAPIView.as_view(), name='update'),
+    path("trainer/<id>/", TrainerRetrieveAPIView.as_view(), name='detail'),
+    path("trainerdelate/<id>/", TrainerDestroyAPIView.as_view(), name='delate'),
+
+    path("clientcreated/", ClientCreateAPIView.as_view(), name='trainer-created'),
+    path("clientupdate/<id>/", ClientUpdateAPIView.as_view(), name='update'),
+    path("client/<id>/", ClientRetrieveAPIView.as_view(), name='detail'),
+    path("clientdelate/<id>/", ClientDestroyAPIView.as_view(), name='delate'),
+ 
+    path("workoutsessioncreated/", WorkoutSessionCreateAPIView.as_view(), name='trainer-created'),
+    path("workoutsessionupdate/<id>/", WorkoutSessionUpdateAPIView.as_view(), name='update'),
+    path("workoutsession/<id>/", WorkoutSessionRetrieveAPIView.as_view(), name='detail'),
+    path("workoutsessiondelate/<id>/", WorkoutSessionDestroyAPIView.as_view(), name='delate'),
+
+
+
+
 ]
--- my_app/views.py
+++ my_app/views.py
@@ -29,3 +29,73 @@ class GYMDestroyAPIView(DestroyAPIView):
     queryset = GYM.objects.all()
     serializer_class = GYMSerializer
     lookup_field = 'id'
+
+class TrainerListView(ListAPIView):
+    queryset = Trainer.objects.all()
+    serializer_class = TrainerSerializer
+
+class TrainerCreateAPIView(CreateAPIView):
+    queryset = Trainer.objects.all()
+    serializer_class = TrainerSerializer
+
+class TrainerUpdateAPIView(UpdateAPIView):
+    queryset = Trainer.objects.all()
+    serializer_class = TrainerSerializer
+    lookup_field = 'id'
+
+class TrainerRetrieveAPIView(RetrieveAPIView):
+    queryset = Trainer.objects.all()
+    serializer_class = TrainerSerializer
+    lookup_field = 'id'
+
+class TrainerDestroyAPIView(DestroyAPIView):
+    queryset = Trainer.objects.all()
+    serializer_class = GYMSerializer
+    lookup_field = 'id'
+
+class ClientListView(ListAPIView):
+    queryset = Client.objects.all()
+    serializer_class = ClientSerializer
+
+class ClientCreateAPIView(CreateAPIView):
+    queryset = Client.objects.all()
+    serializer_class = ClientSerializer
+
+class ClientUpdateAPIView(UpdateAPIView):
+    queryset = Client.objects.all()
+    serializer_class = ClientSerializer
+    lookup_field = 'id'
+
+class ClientRetrieveAPIView(RetrieveAPIView):
+    queryset = Client.objects.all()
+    serializer_class = ClientSerializer
+    lookup_field = 'id'
+
+class ClientDestroyAPIView(DestroyAPIView):
+    queryset = Client.objects.all()
+    serializer_class = ClientSerializer
+    lookup_field = 'id'
+
+class WorkoutSessionListView(ListAPIView):
+    queryset = WorkoutSession.objects.all()
+    serializer_class = WorkoutSessionSerializer
+
+class WorkoutSessionCreateAPIView(CreateAPIView):
+    queryset = WorkoutSession.objects.all()
+    serializer_class = WorkoutSessionSerializer
+
+class WorkoutSessionUpdateAPIView(UpdateAPIView):
+    queryset = WorkoutSession.objects.all()
+    serializer_class = WorkoutSessionSerializer
+    lookup_field = 'id'
+
+class WorkoutSessionRetrieveAPIView(RetrieveAPIView):
+    queryset = WorkoutSession.objects.all()
+    serializer_class = WorkoutSessionSerializer
+    lookup_field = 'id'
+
+class WorkoutSessionDestroyAPIView(DestroyAPIView):
+    queryset = WorkoutSession.objects.all()
+    serializer_class = WorkoutSessionSerializer
+    lookup_field = 'id'
+
